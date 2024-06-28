ecfff61d7274e09eefa49a7e8a37cd9a864869f0
Shaked Hagag <shaked.hagag@openweb.com>
2024-06-28 16:33:36 +0300

    icon styles validate request func

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,8 @@ import { db } from "./db/index";
 import { sessions, users } from "./db/schema";
 import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
 import { UserId as CustomUserId } from "@/lib/types/";
+import { cookies } from "next/headers";
+import { Session, User } from "lucia";
 
 const adapter = new DrizzleSQLiteAdapter(db, sessions, users);
 
@@ -19,6 +21,41 @@ export const lucia = new Lucia(adapter, {
     };
   },
 });
+
+export const validateRequest = async (): Promise<
+  { user: User; session: Session } | { user: null; session: null }
+> => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const result = await lucia.validateSession(sessionId);
+
+  // next.js throws when you attempt to set cookie when rendering page
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+  } catch {}
+  return result;
+};
 
 declare module "lucia" {
   interface Register {
